333bdeb6e3a2c58898cee2804f0a7b11a9d2b146
Fasil <fasilvaliyattil843@gmail.com>
2023-12-27 10:58:17 +0530

    token exp time updated

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,14 +1,9 @@
 export const OTP_TIMER = 1000 * 60 * 3  
 export const TheaterShowLimit = 6
 export const maxDistance = 100 // users can see theaters in 100 km radius
-export const accessTokenExp = 15 * 60 // 15 min
-export const refreshTokenExp = 3 * 60 * 60 // 3 hour
+export const accessTokenExp = 3 * 60 * 60 // 3 hour
+export const refreshTokenExp = 24 * 60 * 60 // 24 hour
 export const tempTokenExp = 10 * 60 // 10 min
-
-// const now = new Date()
-// export const FullRefundTime = new Date(now.setDate(now.getDate() - 3)); // 3 Days before
-// export const HalfRefundTime = new Date(now.setDate(now.getDate() - 1)) // Yesterday
-// export const QuarterRefundTime = new Date(now.setHours(now.getHours() - 4)) // 4 Hour Before
 
 // Time is in hours
 export const ThreeQuarterRefundTime = 48
